7e0a5e864a46307ee0ee9f1b3ca23da2b4ea6d53
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2020-01-10 12:22:02 +0200

    Shadow component

---
+++ src/Screens/components/Shadow.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import RN from 'react-native';
+
+interface Props {
+  style?: RN.ViewStyle;
+  shadowRadius?: number; // in range [1, 16]
+}
+
+function interpolate(input: number, inputRange: [number, number], outputRange: [number, number]): number {
+    const inputRangeLen = inputRange[1] - inputRange[0];
+    const outputRangeLen = outputRange[1] - outputRange[0];
+
+    const multiplier = outputRangeLen / inputRangeLen;
+
+    const value = input - inputRange[0];
+
+    return value * multiplier;
+}
+
+
+const Shadow: React.FC<Props> = ({ style, shadowRadius: value, children }) => {
+  const shadowRadius = value ?? 7;
+  const shadowRadiusRange: [number, number] = [1, 16];
+  const elevation = interpolate(shadowRadius, shadowRadiusRange, [1, 24]);
+  const shadowOffset = {
+    width: 0,
+    height: interpolate(shadowRadius, shadowRadiusRange, [1, 12]),
+  };
+  const shadowOpacity = interpolate(shadowRadius, shadowRadiusRange, [0.18, 0.58]);
+
+  const shadowStyle = {
+      shadowColor: "#000",
+      shadowRadius,
+      elevation,
+      shadowOffset,
+      shadowOpacity
+  };
+
+  return (<RN.View
+      style={[shadowStyle, style]}
+    >{children}</RN.View>)
+}
+
+export default Shadow;
