26e154ddef7480c24cf75027cf40e750c9841d33
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2020-01-27 15:24:14 +0200

    MentorList component

--- src/Screens/Onboarding/MentorList.tsx
+++ src/Screens/Onboarding/MentorList.tsx
@@ -2,21 +2,18 @@ import React from 'react';
 import RN from 'react-native';
 import * as redux from 'redux';
 import * as ReactRedux from 'react-redux';
-import * as snapCarousel from 'react-native-snap-carousel';
 import { SafeAreaView } from 'react-navigation';
 
 import * as navigationProps from '../../lib/navigation-props';
 import * as remoteData from '../../lib/remote-data';
-import useLayout from '../../lib/use-layout';
 import * as api from '../../api/mentors';
 import * as state from '../../state';
 
+import MentorListComponent from '../components/MentorList';
 import Background from '../components/Background';
-import MentorCard from '../components/MentorCard';
 import colors from '../components/colors';
 import fonts from '../components/fonts';
 import { textShadow } from '../components/shadow';
-import RemoteData from '../components/RemoteData';
 import Button from '../components/Button';
 import Message from '../components/Message';
 import CreatedBySosBanner from '../components/CreatedBySosBanner';
@@ -37,9 +34,6 @@ type OwnProps = navigationProps.NavigationProps<MentorListRoute, SignUpRoute>;
 type Props = StateProps & DispatchProps & OwnProps;
 
 const MentorList = (props: Props) => {
-  const [{ width, height }, onLayout] = useLayout();
-  const measuredWidth = width || RN.Dimensions.get('window').width;
-
   const navigateNext = () => {
     props.navigation.navigate('Onboarding/SignUp', {});
   };
@@ -51,22 +45,10 @@ const MentorList = (props: Props) => {
       >
         <Message style={styles.title1} id="onboarding.mentorlist.upperTitle" />
         <Message style={styles.title2} id="onboarding.mentorlist.lowerTitle" />
-        <RN.View onLayout={onLayout} style={styles.mentorListContainer}>
-          <RemoteData data={props.mentors} fetchData={props.fetchMentors}>
-            {value => (
-              <RN.View style={styles.carouselContainer}>
-                <snapCarousel.default
-                  data={[...value.values()]}
-                  renderItem={renderMentorCard(height)}
-                  sliderWidth={measuredWidth}
-                  itemWidth={measuredWidth * 0.9}
-                  inactiveSlideOpacity={1}
-                  inactiveSlideScale={1}
-                />
-              </RN.View>
-            )}
-          </RemoteData>
-        </RN.View>
+        <MentorListComponent
+          mentors={props.mentors}
+          fetchMentors={props.fetchMentors}
+        />
         <RN.View style={styles.bottom}>
           <Button
             style={styles.button}
@@ -83,19 +65,6 @@ const MentorList = (props: Props) => {
   );
 };
 
-const mentorCardBottomMargin = 16;
-
-const renderMentorCard = (maxHeight: number) => ({
-  item,
-}: {
-  item: api.Mentor;
-}) => (
-  <MentorCard
-    style={[styles.card, { maxHeight: maxHeight - mentorCardBottomMargin }]}
-    mentor={item}
-  />
-);
-
 const styles = RN.StyleSheet.create({
   container: {
     flex: 1,
@@ -110,21 +79,6 @@ const styles = RN.StyleSheet.create({
     ...fonts.specialTitle,
     ...textShadow,
     color: colors.white,
-  },
-  mentorListContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-    alignSelf: 'stretch',
-  },
-  carouselContainer: {
-    flex: 1,
-  },
-  card: {
-    alignSelf: 'stretch',
-    marginHorizontal: 8,
-    flexGrow: 1,
-    marginBottom: mentorCardBottomMargin,
   },
   bottom: {
     marginTop: 8,
---
+++ src/Screens/components/MentorList.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import RN from 'react-native';
+import * as snapCarousel from 'react-native-snap-carousel';
+
+import * as remoteData from '../../lib/remote-data';
+import useLayout from '../../lib/use-layout';
+
+import MentorCard from '../components/MentorCard';
+import RemoteData from '../components/RemoteData';
+
+import * as mentorApi from '../../api/mentors';
+
+type Props = {
+  fetchMentors: () => void | undefined;
+  mentors: remoteData.RemoteData<Map<string, mentorApi.Mentor>>;
+  onPress?: () => void | undefined;
+};
+
+const MentorList = ({ fetchMentors, mentors, onPress }: Props) => {
+  const [{ width, height }, onLayout] = useLayout();
+  const measuredWidth = width || RN.Dimensions.get('window').width;
+  return (
+    <RN.View onLayout={onLayout} style={styles.mentorListContainer}>
+      <RemoteData data={mentors} fetchData={fetchMentors}>
+        {value => (
+          <RN.View style={styles.carouselContainer}>
+            <snapCarousel.default
+              data={[...value.values()]}
+              renderItem={renderMentorCard(height, onPress)}
+              sliderWidth={measuredWidth}
+              itemWidth={measuredWidth * 0.9}
+              inactiveSlideOpacity={1}
+              inactiveSlideScale={1}
+            />
+          </RN.View>
+        )}
+      </RemoteData>
+    </RN.View>
+  );
+};
+
+const mentorCardBottomMargin = 16;
+
+const renderMentorCard = (
+  maxHeight: number,
+  onPress?: () => void | undefined,
+) => ({ item }: { item: mentorApi.Mentor }) => (
+  <MentorCard
+    style={[styles.card, { maxHeight: maxHeight - mentorCardBottomMargin }]}
+    mentor={item}
+    onPress={onPress}
+  />
+);
+
+const styles = RN.StyleSheet.create({
+  mentorListContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  carouselContainer: {
+    flex: 1,
+  },
+  card: {
+    alignSelf: 'stretch',
+    marginHorizontal: 8,
+    flexGrow: 1,
+    marginBottom: mentorCardBottomMargin,
+  },
+});
+
+export default MentorList;
