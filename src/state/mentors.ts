527508ddee7e98e2b94ff602106cfa4118f58520
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2020-03-06 17:02:30 +0200

    tidy mentors

--- src/state/mentors.ts
+++ src/state/mentors.ts
@@ -2,6 +2,7 @@ import * as automaton from 'redux-automaton';
 import * as RD from '@devexperts/remote-data-ts';
 import * as R from 'fp-ts-rxjs/lib/Observable';
 import { pipe } from 'fp-ts/lib/pipeable';
+import { flow } from 'fp-ts/lib/function';
 
 import * as mentorsApi from '../api/mentors';
 
@@ -12,6 +13,11 @@ export type State = model.AppState['mentors'];
 
 export const initialState = RD.initial;
 
+const fetchMentors = flow(
+  mentorsApi.fetchMentors,
+  R.map(actions.creators.fetchMentorsCompleted),
+);
+
 export const reducer: automaton.Reducer<State, actions.Action> = (
   state = initialState,
   action,
@@ -20,12 +26,7 @@ export const reducer: automaton.Reducer<State, actions.Action> = (
     case 'fetchMentors':
       return automaton.loop(
         RD.pending,
-        actions.creators.fetchCmd(() =>
-          R.observable.map(
-            mentorsApi.fetchMentors(),
-            actions.creators.fetchMentorsCompleted,
-          ),
-        ),
+        actions.creators.fetchCmd(fetchMentors),
       );
     case 'fetchMentorsCompleted':
       return pipe(
