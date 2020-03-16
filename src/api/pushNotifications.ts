2860975dd1260927b387b657ea3d5be4612b94b2
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2020-03-16 16:58:31 +0200

    Add permission asking for push notifications
    Only js side.

--- src/api/pushNotifications.ts
+++ src/api/pushNotifications.ts
@@ -1,6 +1,8 @@
 import RN from 'react-native';
 import * as t from 'io-ts';
 import { pipe } from 'fp-ts/lib/pipeable';
+import { Lazy, constant } from 'fp-ts/lib/function';
+import { fold } from 'fp-ts/lib/boolean';
 import * as TE from 'fp-ts/lib/TaskEither';
 import * as RE from 'fp-ts-rxjs/lib/ObservableEither';
 import * as firebase from 'react-native-firebase';
@@ -11,25 +13,40 @@ import * as err from '../lib/http-err';
 import * as config from './config';
 import * as authApi from './auth';
 
-export const getDeviceToken = TE.tryCatch(
-  () => firebase.messaging().getToken(),
-  (reason: unknown) => err.reasonError(reason),
+export const fromPromise: <A>(
+  a: Lazy<Promise<A>>,
+) => RE.ObservableEither<err.Err, A> = a =>
+  pipe(
+    TE.tryCatch(a, (reason: unknown) => err.reasonError(reason)),
+    RE.fromTaskEither,
+  );
+
+export const checkIfHasPermission = fromPromise(() =>
+  firebase.messaging().hasPermission(),
 );
 
-export function putDeviceToken(accessToken: authApi.AccessToken) {
-  return pipe(
-    getDeviceToken,
-    RE.fromTaskEither,
-    RE.chain(deviceToken =>
-      http.validateResponse(
-        http.put(
-          `${config.baseUrl}/users/${accessToken.userId}/device`,
-          { token: deviceToken, platform: RN.Platform.OS },
-          { headers: authApi.authHeader(accessToken) },
-        ),
-        t.unknown,
-        _ => undefined,
+export const requestPermission = fromPromise(() =>
+  firebase.messaging().requestPermission(),
+);
+
+export const getDeviceToken = fromPromise(() =>
+  firebase.messaging().getToken(),
+);
+
+export const requestPermissions = RE.observableEither.chain(
+  checkIfHasPermission,
+  fold(constant(requestPermission), constant(RE.right(undefined))),
+);
+
+export const putDeviceToken = (accessToken: authApi.AccessToken) =>
+  RE.observableEither.chain(getDeviceToken, deviceToken =>
+    http.validateResponse(
+      http.put(
+        `${config.baseUrl}/users/${accessToken.userId}/device`,
+        { token: deviceToken, platform: RN.Platform.OS },
+        { headers: authApi.authHeader(accessToken) },
       ),
+      t.unknown,
+      _ => undefined,
     ),
   );
-}
