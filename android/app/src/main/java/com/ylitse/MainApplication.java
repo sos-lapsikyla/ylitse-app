0177089dccd1d5ee47e447cc295d2be1963cd85f
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2020-03-18 09:27:14 +0200

    Add android and ios notification handling

--- android/app/src/main/java/com/ylitse/MainApplication.java
+++ android/app/src/main/java/com/ylitse/MainApplication.java
@@ -4,6 +4,11 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // FCM
+
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -23,8 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new RNFirebaseMessagingPackage()); // FCM
           return packages;
         }
 
