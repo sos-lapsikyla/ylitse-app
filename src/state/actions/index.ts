2dcb0099bed03b07a92887fb7c18f5bac7347d61
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2020-04-03 11:36:38 +0300

    Remove dead code

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -2,12 +2,10 @@ import * as automaton from 'redux-automaton';
 
 import * as regular from './regular';
 import * as middleware from '../middleware';
-import * as epic from './epic';
 
 export type RegularAction = regular.Action;
 export type Cmd = middleware.Cmd<regular.Action>;
-export type EpicAction = epic.FetchCmd;
-export type Action = regular.Action | epic.FetchCmd | Cmd;
+export type Action = regular.Action | Cmd;
 
 export type LS<S> = S | automaton.Loop<S, Action>;
 export type Reducer<S> = (state: S | undefined, action: Action) => LS<S>;
