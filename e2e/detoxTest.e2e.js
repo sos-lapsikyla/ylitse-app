3abba4950e90eb4c8f3b502b50002757e078d513
ziz <ahto.jussila@gmail.com>
2020-11-15 10:36:23 +0200

    Convert Detox setup to use TypeScript

--- e2e/detoxTest.e2e.js
+++
@@ -1,9 +0,0 @@
-describe('Detox', () => {
-    beforeEach(async () => {
-      await device.reloadReactNative();
-    });
-    
-    it('finds a word from first screen', async () => {
-      await expect(element(by.text('Start'))).toBeVisible();
-    });
-});
---
+++ e2e/smokeTest.spec.ts
@@ -0,0 +1,11 @@
+import { by, element, expect, device } from "detox"
+
+describe('Detox', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  })
+
+  it('finds a word from first screen', async () => {
+    await expect(element(by.text('Start'))).toBeVisible();
+  });
+});
