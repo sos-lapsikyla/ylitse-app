e9dda2612bbbda95a6633fb39b5c4eb7df8a49ec
Ahto Jussila <ahto.jussila@gmail.com>
2020-11-29 22:12:00 +0200

    Fail fast if cannot connect to API

--- e2e/init.ts
+++ e2e/init.ts
@@ -7,10 +7,19 @@ const config = require('../.detoxrc.json');
 import * as j from '@jest/globals';
 declare const jasmine: Jasmine;
 
+import { APIAdminAccessToken } from './helpers';
+
 j.jest.setTimeout(240000);
 jasmine.getEnv().addReporter(adapter);
 
 j.beforeAll(async () => {
+  try {
+    await APIAdminAccessToken();
+  } catch (error) {
+    console.error("Cannot connect to YLITSE API. YLITSE_API_ADMIN_LOGIN_NAME=... YLITSE_API_ADMIN_PASSWORD=... YLITSE_API_URL=...");
+    process.exit(1);
+  }
+
   await init(config, { initGlobals: false });
 });
 
