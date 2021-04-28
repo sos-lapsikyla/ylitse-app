a02f174d0e4ac5e7956927d3206854d2706c4185
Felix Hallenberg <feqwin@gmail.com>
2021-04-28 17:12:02 +0300

    Create translations

--- src/localization/fi.ts
+++ src/localization/fi.ts
@@ -37,6 +37,8 @@ export const messages = {
   'main.chat.ban': 'Estä keskustelu',
   'main.chat.ban.confirmation': 'Haluatko varmasti estää tämän keskustelun?',
   'main.chat.navigation.banned': 'Estetyt',
+  'main.chat.unban': 'Palauta keskustelu',
+  'main.chat.unban.confirmation': 'Haluatko palauttaa tämän keskustelun?',
 
   'main.mentor.other': 'Pystyn auttamaan näissä:',
   'main.mentor.story': 'Tarinani',
