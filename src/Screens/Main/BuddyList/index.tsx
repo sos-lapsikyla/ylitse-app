add0449671caec891a65a696fb071d75f796560a
Felix Hallenberg <feqwin@gmail.com>
2021-05-22 07:27:32 +0300

    Fix e2e and only show one modal at a time

--- src/Screens/Main/BuddyList/index.tsx
+++ src/Screens/Main/BuddyList/index.tsx
@@ -60,6 +60,7 @@ export default ({ navigation }: Props) => {
             style={styles.kebab}
             onPress={() => setDropdownOpen(!isDropdownOpen)}
             underlayColor={colors.faintBackground}
+            testID={'main.buddylist.kebabicon'}
           >
             <RN.Image
               source={require('../../images/three-dot-menu.svg')}
