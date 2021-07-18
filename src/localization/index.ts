5e11f421a8ae9fc4d6a9c43572515cf651dd9460
Osamah Amer <osamah.amer@learnit.fi>
2021-07-18 17:51:05 +0300

    added arabic language

--- src/localization/index.ts
+++ src/localization/index.ts
@@ -1,21 +1,18 @@
 // TODO get language from redux/somewhere
 // Desing todo, how is default language chosen?
 //
-import isFinnishPhone from '../lib/isFinnishPhone';
 
 import * as fi from './fi';
 import * as en from './en';
-
+import * as ar from './ar';
+import systemLanguage from '../lib/systemLanguage';
 export type MessageId = fi.MessageId;
-
-type Language = 'fi' | 'en';
-
-const lang: Language = isFinnishPhone ? 'fi' : 'en';
 
 const messages = {
   fi: fi.messages,
   en: en.messages,
+  ar: ar.messages,
 };
-export const trans = (k: MessageId) => messages[lang][k];
+export const trans = (k: MessageId) => messages[systemLanguage][k];
 
 export const blank: MessageId = 'meta.blank';
