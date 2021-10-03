17079fdfea532e3d49da11c0ae69816151acbe39
Felix Hallenberg <feqwin@gmail.com>
2021-10-03 19:22:32 +0300

    Fix status_message InputField styles

--- src/Screens/components/InputField.tsx
+++ src/Screens/components/InputField.tsx
@@ -5,7 +5,7 @@ import fonts from './fonts';
 import colors from './colors';
 
 export interface Props extends RN.TextInputProps {
-  inputStyle?: RN.StyleProp<RN.ViewStyle>;
+  inputStyle?: RN.StyleProp<RN.TextStyle>;
 }
 
 const InputField = ({
@@ -17,34 +17,41 @@ const InputField = ({
   ...textInputProps
 }: Props) => {
   return (
-    <RN.View style={style}>
-      <RN.View>
-        <RN.TextInput
-          style={[inputStyle, styles.inputText]}
-          maxLength={maxLength}
-          multiline={multiline}
-          numberOfLines={numberOfLines}
-          editable={true}
-          {...textInputProps}
-        />
-      </RN.View>
+    <RN.View style={[styles.container, style]}>
+      <RN.TextInput
+        style={[styles.inputText, inputStyle]}
+        maxLength={maxLength}
+        multiline={multiline}
+        numberOfLines={numberOfLines}
+        editable={true}
+        {...textInputProps}
+      />
     </RN.View>
   );
 };
 
 const styles = RN.StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    backgroundColor: colors.lightestGray,
+    borderRadius: 16,
+    marginVertical: 8,
+  },
   inputText: {
-    ...fonts.regularBold,
+    ...fonts.regular,
     color: colors.darkestBlue,
     backgroundColor: colors.lightestGray,
-    alignSelf: 'stretch',
     flex: 1,
     flexGrow: 1,
-    paddingVertical: 8,
-    paddingLeft: 16,
-    paddingRight: 48,
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    alignSelf: 'stretch',
+    paddingHorizontal: 16,
+    paddingVertical: 16,
     borderRadius: 16,
-    textAlignVertical: 'top',
+    marginTop: RN.Platform.OS === 'ios' ? 8 : undefined,
   },
 });
 
