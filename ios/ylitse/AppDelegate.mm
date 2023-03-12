3a434db494b902ecf1a724e52ca3658ca7c1ddf0
Felix Hallenberg <feqwin@gmail.com>
2023-03-12 10:10:37 +0200

    Runs in iOS too

--- ios/ylitse/AppDelegate.mm
+++ ios/ylitse/AppDelegate.mm
@@ -1,14 +1,25 @@
+#import <Firebase.h>
+
 #import "AppDelegate.h"
+
 #import <React/RCTBundleURLProvider.h>
+
+
+
 @implementation AppDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"ylitse";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  [FIRApp configure];
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
@@ -17,6 +28,7 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+
 /// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
 ///
 /// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
