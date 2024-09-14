c60ac1296406ab0b2d2b89280f115c2f2f6e77a3
Felix Hallenberg <feqwin@gmail.com>
2024-09-14 11:55:56 +0300

    Can launch android in debug-mode

--- android/app/src/main/java/com/ylitse/MainApplication.kt
+++ android/app/src/main/java/com/ylitse/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rndiffapp
+package com.ylitse
  
 import android.app.Application
 import com.facebook.react.PackageList
--- src/state/index.ts
+++ src/state/index.ts
@@ -4,15 +4,10 @@ import * as redux from 'redux';
 import * as types from './types';
 import * as reducers from './reducers';
 import * as middleware from './middleware';
-import createDebugger from 'redux-flipper';
 
 export type AppState = types.AppState;
 
 const middlewares = [middleware.taskRunner];
-
-if (__DEV__) {
-  middlewares.push(createDebugger());
-}
 
 const enhancer = redux.compose(redux.applyMiddleware(...middlewares));
 
